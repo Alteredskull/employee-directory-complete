24b375457d71102a71d92e59b593d81c9ebbf7d9
Alteredskull <johnson.diantai@gmail.com>
2020-05-16 10:02:41 -0400

    Complete Reupload
    Upload

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../styles/Header.css";
+import "../styles/Neon.css";
+
+function Header () {
+    return (
+      <div className="header">
+        <h1 className="sign">Not Your Typical Employee Directory :P</h1>
+        <p>Find them here, quick and easy!</p>
+      </div>
+    )
+  }
+
+export default Header;
