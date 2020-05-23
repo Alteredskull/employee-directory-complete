f2b33dea4cad0d379178601d5f5b7fa782ef3055
Alteredskull <johnson.diantai@gmail.com>
2020-05-23 12:24:30 -0400

    Updates

---
+++ precache-manifest.99e1b6ed72e69c15b81b9626f245d41b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede7efdebba82513d94bd3b86a94bb49",
+    "url": "/github.io/employee-directory-complete/index.html"
+  },
+  {
+    "revision": "5c5f97810eff963b28ac",
+    "url": "/github.io/employee-directory-complete/static/css/main.4df86c26.chunk.css"
+  },
+  {
+    "revision": "ff7b9a43d7df22b64daf",
+    "url": "/github.io/employee-directory-complete/static/js/2.7f2b47ec.chunk.js"
+  },
+  {
+    "revision": "5c5f97810eff963b28ac",
+    "url": "/github.io/employee-directory-complete/static/js/main.81278603.chunk.js"
+  },
+  {
+    "revision": "0a72cb6542f0c7328d63",
+    "url": "/github.io/employee-directory-complete/static/js/runtime-main.a322ca86.js"
+  }
+]);
